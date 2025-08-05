03f55afd2f04245b4e0f95c034a3ba9f0da33447
nfranks8036 <nfranks8036@gmail.com>
2025-08-05 15:44:01 -0400

    August 5th, 2025 @ 15:44

--- common/src/main/java/net/noahf/firewatch/common/loader/JsonImporter.java
+++ common/src/main/java/net/noahf/firewatch/common/loader/JsonImporter.java
@@ -1,9 +1,10 @@
 package net.noahf.firewatch.common.loader;
 
+import net.noahf.firewatch.common.incidents.NewIncidentType;
+
 public class JsonImporter {
 
     public JsonImporter() {
-
     }
 
 }
