dcdaff27e414743aad3e5207ee5a6a7d374cf873
CyberedCake <nfranks8036@gmail.com>
2026-06-24 00:22:09 -0400

    Add state in agencies text

--- FireGenDiscord/src/main/java/net/noahf/firegen/discord/command/registered/Agencies.java
+++ FireGenDiscord/src/main/java/net/noahf/firegen/discord/command/registered/Agencies.java
@@ -2,6 +2,7 @@ package net.noahf.firegen.discord.command.registered;
 
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.noahf.firegen.api.incidents.SystemMunicipality;
 import net.noahf.firegen.api.incidents.units.Agency;
 import net.noahf.firegen.discord.Main;
 import net.noahf.firegen.discord.command.Command;
@@ -23,11 +24,12 @@ public class Agencies extends Command {
     @Override
     public void command(SlashCommandInteractionEvent event) {
         List<Agency> agencies = new ArrayList<>(Main.incidents.getAgencies());
+        SystemMunicipality local = Main.incidents.getMunicipality();
         event.replyEmbeds(new EmbedBuilder()
                 .setColor(new Color(128, 252, 222))
                 .setTitle("Agencies (" + agencies.size() + ")")
                 .setDescription(
-                        "These agencies serve " + Main.incidents.getMunicipality().getName() + ":\n\n" +
+                        "These agencies serve " + local.getName() + ", " + local.getState().getAbbreviation() + ":\n\n" +
                         agencies.stream().map(a -> (AgencyImpl) a).map(a ->
                                 "- " + (a.getEmoji() != null ? a.getEmoji().getFormatted() + " " : "") + a.getFormatted()
                                 + " (`" + a.getShorthand() + "`)"
